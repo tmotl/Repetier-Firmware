b909bab89e43e6660429c29fb2c3ae880167d592
RF1000 <rf1000@gmx.net>
2016-05-19 10:17:15 +0200

    OTHER - The amount of time for which the precise heat bed scan waits for the heating up has been increased. - Moving to RF.01.23.

--- RF1000/Repetier/RF2000.h
+++ RF1000/Repetier/RF2000.h
@@ -1005,8 +1005,8 @@ Above this value the z compensation will distribute the roughness of the surface
 
 #if FEATURE_PRECISE_HEAT_BED_SCAN
 
-#define PRECISE_HEAT_BED_SCAN_WARMUP_DELAY			30																		// [s]
-#define PRECISE_HEAT_BED_SCAN_CALIBRATION_DELAY		30																		// [s]
+#define PRECISE_HEAT_BED_SCAN_WARMUP_DELAY			(unsigned long)600														// [s]
+#define PRECISE_HEAT_BED_SCAN_CALIBRATION_DELAY		(unsigned long)600														// [s]
 #define PRECISE_HEAT_BED_SCAN_BED_TEMP_PLA			60																		// [°C]
 #define PRECISE_HEAT_BED_SCAN_BED_TEMP_ABS			120																		// [°C]
 #define	PRECISE_HEAT_BED_SCAN_EXTRUDER_TEMP_SCAN	100																		// [°C]
